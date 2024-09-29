2b436b371dc469306044cd19f0ca312ccd49082f
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-09-29 12:09:29 +0500

    git html element through js (date, calBtn) apply event on it.

---
+++ script.js
@@ -0,0 +1,11 @@
+const date = document.getElementById('date');
+const calculateBtn = document.getElementById('CalculateBtn');
+
+// console.log(date)
+// console.log(CalculateBtn)
+
+calculateBtn.addEventListener('click', () => {
+  // git birthday value
+  const birthday = date.vlue;
+  console.log(birthday);
+});
