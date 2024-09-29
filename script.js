a92ebbc065ac6514d486d26c1495c8c84741e647
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-09-29 12:28:29 +0500

    as

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ const calculateBtn = document.getElementById('CalculateBtn');
 // console.log(CalculateBtn)
 
 calculateBtn.addEventListener('click', () => {
+
+  // git display box
+  const box1 = document.getElementById('box1')
+  const box2 = document.getElementById('box2')
+  const box3 = document.getElementById('box3')
+
   // git birthday value
-  const birthday = date.vlue;
-  console.log(birthday);
+  const birthday = date.value;
+  // console.log(birthday);
+
+  // create new date Object
+  const birthdayDate = new Date(birthday)
+  // console.log(birthdayDate)
+
+  // current date 
+  const curretDate = new Date();
+  // console.log(curretDate)
+
+
+
+
+
 });
